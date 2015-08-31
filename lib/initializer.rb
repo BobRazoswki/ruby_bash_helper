47c2d0aaf442de5fef1fe3021ea35eb15af22796
Bob <bobongithub@gmail.com>
2015-08-31 20:29:01 +0200

    Update initializer.rb

--- lib/initializer.rb
+++ lib/initializer.rb
@@ -1,7 +1,7 @@
 require_relative "automatizer"
-puts "May I ask you to specify the name of the project you want to open sir ?"
+puts "May I ask you to specify the name of the project you want to start sir ?"
 value = STDIN.gets.chomp
-puts "Thanks you, your request is processing"
+puts "Thanks you sir, your request is processing"
 Automatizer.run_gulp value
 Automatizer.run_vim value
 Automatizer.root_directory value
